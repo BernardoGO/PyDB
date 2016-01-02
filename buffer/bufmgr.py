35cb119b53db3bd1fda849e6fb2c9933cc9e4fef
bernardogo <bernardo.godinho.oliveira@gmail.com>
2016-01-02 05:00:49 -0600

    Removing circular dependency

--- buffer/bufmgr.py
+++ buffer/bufmgr.py
@@ -3,7 +3,6 @@ __author__ = 'Bernardo'
 from catalog.core import catalogCore
 from storage.io import general
 from storage.tablemgr import manager
-from storage.pageManager import pageManager
 import calendar
 import time
 
@@ -14,22 +13,48 @@ class buffer:
         self.pid = None
         self.page = None
         self.timestamp = None
+        self.pinCount = 0
+        self.dirty = False
 
 
 class buffer_pool:
+    pool = []
     def __init__(self):
-        self.pool = []
+
         for x in range(0, __MAX_SIZE__):
             self.addToPool(None)
 
+    def findPage(self, pid):
+        pos = -1
+        for x in range(len(buffer_pool.pool)):
+            if buffer_pool.pool[x].pid == pid:
+                pos = x
+                break
+        return pos
 
     def addToPool(self, pid):
-        pgmg = pageManager()
         ios = general()
         newb = buffer()
         newb.pid = pid
         newb.timestamp = calendar.timegm(time.gmtime())
         if pid is not None:
             newb.page = ios.readPage(pid)
-        self.pool.append(newb)
+        buffer_pool.pool.append(newb)
+
+    def findVictimPage(self):
+        victim = 0
+        for x in range(len(buffer_pool.pool)):
+            if buffer_pool.pool[x].timestamp < victim.timestamp and buffer_pool.pool[x].pincount == 0:
+                victim = x
+        return victim
+
+    def replacePage(self, pid):
+        ios = general()
+        newb = buffer()
+        newb.pid = pid
+        newb.timestamp = calendar.timegm(time.gmtime())
+        newb.page = ios.readPage(pid)
+        victim = self.findVictimPage()
+        buffer_pool.pool[victim] = newb
+
 
